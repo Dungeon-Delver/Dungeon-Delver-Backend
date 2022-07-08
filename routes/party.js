61001cdd8b11a2262ab81786c0eca585220220db
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-08 14:15:49 -0700

    Started initializing creating party

--- routes/party.js
+++ routes/party.js
@@ -5,11 +5,22 @@ const { NotFoundError } = require("../utils/errors")
 
 
 
-  router.get("/party/:page", async (req, res, next) => {
+router.get("/:page", async (req, res, next) => {
     try {
         const pageNumber = req.params.page
         //const orders = await Store.listProductsByPage(pageNumber-1)
         res.status(200).json({ orders })
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
+router.post("/create-party", async (req, res, next) => {
+    try {
+        const body = req.body;
+        const newParty = await Party.handleCreateParty(body)
+        res.status(201).json({ newParty })
     }
     catch(err) {
         next(err)
