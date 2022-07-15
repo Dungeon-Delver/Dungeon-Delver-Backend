eaa4c0614627b8fd83240d5e3c51eb377ba42359
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-15 11:22:54 -0700

    Refactored listParties() to also return party members

--- models/user.js
+++ models/user.js
@@ -4,17 +4,32 @@ const Parse = require("../utils/initializeParse")
 
 class User {
   static async listParties(userId) {
+
+    const getMembers = async (parties) => {
+        const newParties = await Promise.all(parties.map( async item => {
+        const dmObj = await item.get("dm")
+        const dmId = dmObj.id
+        const dmQuery = new Parse.Query("User")
+        const dm = await dmQuery.get(dmId)
+        const players = await item.get("players").query().find()
+        return {party: item, dm: dm, players: players};
+      }))
+      return newParties
+    }
+
     const Parties = Parse.Object.extend("Party");
     const dmQuery = new Parse.Query(Parties);
     const dmParties = await dmQuery.equalTo("dm", { '__type': 'Pointer', 'className': '_User', 'objectId': userId }).find();
-    
+    const dmPartiesRet =  await getMembers(dmParties)
+
     const playerQuery = new Parse.Query(Parties);
     const userQuery = new Parse.Query("User")
     const user = await userQuery.get(userId)
 
     const playerParties = await playerQuery.equalTo("players", user).find()
-    
-    return {dmParties: dmParties, playerParties: playerParties};
+    const playerPartiesRet = await getMembers(playerParties)
+
+    return {dmParties: dmPartiesRet, playerParties: playerPartiesRet};
   }
 
   static async requestPartyJoin(userId, partyId) {
