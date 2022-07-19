df49f0408d8e2f33af2e623db439bb9e738ce640
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-18 22:32:36 -0700

    Implementation: Allowed users to cancel join requests (copy and paste of leaving party with one modified line)

--- models/user.js
+++ models/user.js
@@ -72,6 +72,27 @@ class User {
     playersRelation.remove(player)
     party.save()
   }
+
+  static async cancelJoin(userId, partyId) {
+    const Parties = Parse.Object.extend("Party")
+    const partyQuery = new Parse.Query(Parties)
+    const party = await partyQuery.get(partyId)
+
+    const playerQuery = new Parse.Query("User")
+    const player = await playerQuery.get(userId)
+
+    /*Will want to delete notification
+    const notification = new Parse.Object("Notification");
+    const dm = party.get("dm")
+    notification.set("user", dm)
+    notification.set("type", "joinRequest")
+    notification.set("sourceUser", player)
+    notification.save();*/
+
+    let playersRequestedRelation = party.relation('playersRequested')
+    playersRequestedRelation.remove(player)
+    party.save()
+  }
 }
 
 module.exports = User
